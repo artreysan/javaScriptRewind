04ad5e350860a49724fe4af856dbaa895bc3332d
artreysan <70738181+beartree-ars@users.noreply.github.com>
2022-08-14 22:46:23 -0500

    JavaScript Funtion

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("Hola mundo");
+function hello(){
+    console.log("hola mundo");
+    console.log('work with JS');
+}
+
+function hello2(){
+    return 'hola return';
+}
+function helloBool(){
+    return true
+}
+function helloArr(){
+    return [1, 2,2 ];
+
+}
+//Object
+function helloObj(){
+    return {name:ryan}
+}
+//funtion returned
+ function helloReturn(){
+    return function () {
+        return 'hola mundo returnada';
+    }
+ }
+
+hello();
+const result = hello2();
+console.log(result);
+console.log(hello2());
+console.log(helloBool());
+console.log(helloArr());
+//Fundamentals fo Funtional Programming
+console.log(helloReturn());
+console.log(helloReturn()());
