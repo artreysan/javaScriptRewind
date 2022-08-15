12b2081c7cde61bb4f71114ec286ce3e0375a91a
artreysan <70738181+beartree-ars@users.noreply.github.com>
2022-08-15 00:02:51 -0500

    Objects as parameters

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
-//DOM manipulation
+//Objects as Parameters
+const user = {
+    name:'Joe',
+    age:30
+}
 
-//Created a element h1
-//"createElement" is a built-in funtion in JS
-const title = document.createElement('h1')
-//Show in console
-console.log(title)
-//Add a text to the element h1: 'Hola mundo desde JS'
-title.innerHTML='Hola mundo desde JS'
-document.body.append(title)
-
-//Button
-const button = document.createElement('button')
-button.innerHTML = 'click here'
-//Event handlers (Manejador de eventos)
-button.addEventListener('click', function (){
-    console.log('Clicked the button');    
-});
-//Other button
-const button2 = document.createElement('button')
-button2.innerHTML = 'change title'
-button2.addEventListener('click',function () {
-    title.innerHTML = 'Texto actualizado con JS'
-    alert('se realizo un click');
-})
-document.body.append(button)
-document.body.append(button2)
+function printInfo(user){
+    return '<h1> Hola ' + user.name + ' </h1>'    
+}
+console.log(printInfo(user))
+document.body.innerHTML = printInfo(user);
