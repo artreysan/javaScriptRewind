d1cb3b1d58fde692ad49ab51aeaf6c4f1058d6dc
artreysan <70738181+beartree-ars@users.noreply.github.com>
2022-08-15 17:40:26 -0500

    Anonymous Functions

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-//Destructuring 
-const user = {
-    name:'Joe',
-    age:30
+//Anonymous Functions
+//Funcion establecida
+function start(){
+    return "Starting ... "
 }
-//Espera un objeto que tenga el atributo "name"
-function printInfo({name}){
-    return '<h1> Hola ' + name + ' </h1>'    
-}
-console.log(printInfo(user))
-document.body.innerHTML = printInfo(user);
+
+console.log(start())
+
+//Funcion anonima
+console.log(function (){
+    return "Starting anonymous"
+}())
+
+//Example
+//Mode 1 without anonymous function
+const button1 = document.createElement('button');
+button1.innerText = 'click me 1'
+
+function handleClick(){
+    alert('clicked 1');
+} 
+button1.addEventListener('click',handleClick);
+document.body.append(button1)
+
+//Mode 2 with anonymous fucntion
+const button2 = document.createElement('button');
+button2.innerText = 'click me 2'
+button2.addEventListener('click', function(){
+    alert('clicked 2')
+})
+document.body.append(button2);
