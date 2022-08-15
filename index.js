13fced2c56108225da06a9bc7a68eab92db73cae
artreysan <70738181+beartree-ars@users.noreply.github.com>
2022-08-14 23:55:09 -0500

    DOM manipulation

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-//Shorthand property names (Nombres abreviados  de propiedad)
-const name = 'laptop'
-const price = 3000
+//DOM manipulation
 
-//Method 1
-const nameProduct1 = {
-    name:name,
-    price:price
-}
+//Created a element h1
+//"createElement" is a built-in funtion in JS
+const title = document.createElement('h1')
+//Show in console
+console.log(title)
+//Add a text to the element h1: 'Hola mundo desde JS'
+title.innerHTML='Hola mundo desde JS'
+document.body.append(title)
 
-//Method 2
-const nameProduct2 = {
-    name,
-    price
-}
-console.log(nameProduct1);
-console.log(nameProduct2);
+//Button
+const button = document.createElement('button')
+button.innerHTML = 'click here'
+//Event handlers (Manejador de eventos)
+button.addEventListener('click', function (){
+    console.log('Clicked the button');    
+});
+//Other button
+const button2 = document.createElement('button')
+button2.innerHTML = 'change title'
+button2.addEventListener('click',function () {
+    title.innerHTML = 'Texto actualizado con JS'
+    alert('se realizo un click');
+})
+document.body.append(button)
+document.body.append(button2)
