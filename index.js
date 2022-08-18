baf3932e029b6636ca6c615a0c14ce581d8d61d8
artreysan <70738181+beartree-ars@users.noreply.github.com>
2022-08-18 13:40:49 -0500

    Inline Arrow Functions

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
-//Arrow Functions
+//Inline Arrow Functions
 
 //Example 
-function add1(x,y){
-    return x+y
-}
 
-const add2 = (x,y)=>{
-    return x+y
-}
-console.log(add1(5,5));
-console.log(add2(2,2));
+//retorna un texto "Hola mundo"
+const showText = () => "Hola mundo"
+//Retorna un numero
+const showNumber = () => 22;
+//Retorn un boolean 
+const showBoolean = () => true;
+//Retorna un Array
+const showArray = () => [1,2,3]
+//Retorna un objeto
+const showObject = () => ({name:'Ryan',lastname:'Juarez'}) 
 
-//Mode 1
-const button1 = document.createElement('button');
-button1.innerText = 'click me 1'
-button1.addEventListener('click', function(){
-    alert('clicked 2')
-})
-document.body.append(button1)
-
-//Mode 2 
-const button2 = document.createElement('button');
-button2.innerText = 'click me 2'
-button2.addEventListener('click', () =>{
-    alert('clicked 2')
-})
-document.body.append(button2);
+console.log(showText());
+console.log(showNumber());
+console.log(showBoolean());
+console.log(showArray());
+console.log(showObject());
