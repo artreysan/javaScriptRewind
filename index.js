e33239314129c0dd4e44644f01c8e264bba9a025
artreysan <70738181+beartree-ars@users.noreply.github.com>
2022-08-19 11:35:15 -0500

    Array methods

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
-//String literals
+//Arrays methods 
 
-const background = 'yellow';
-const color = 'black';
-const isAuthorized = true;
+const names = ['ryan','joe','maria']
 
-const button = document.createElement('button');
-button.innerText = 'click me'
-button.style = `background: ${isAuthorized === true ? 'green': 'red'}; color: ${color}`
+//Method 1 
+for (let i = 0; i < names.length; i++){
+    const element = names[i];
+    console.log(element);
+}
 
-
-button.addEventListener('click', () =>{
-    if(isAuthorized){
-        return alert('is Authorized')
-    }
-    
-    alert('is not Authorized')
+//Method 1 with  forEach()
+console.log('============================================')
+names.forEach(function(name){
+    console.log(name)
 })
 
-document.body.append(button)
+//Method 2 with MAP
+console.log('============================================')
+const newNames = names.map(function(name){
+    return `Hola ${name}`
+})
+console.log(newNames)
+
+//Method 3 with FIND
+console.log('============================================')
+const nameFound =  names.find(function (name){
+    if(name === 'joe'){
+        return name
+    }
+})
+console.log(nameFound)
+
+//Method 4 with FILTER
+console.log('============================================')
+const nameFilter =  names.filter(function (name){
+    if(name !== 'joe'){
+        return name
+    }
+})
+console.log(nameFilter)
+
+//Method 5 with CONCAT 
+console.log('============================================')
+const newNamess = ['marco', 'mario', 'john']
+console.log(names.concat(newNamess))
+
+// More information: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array 
