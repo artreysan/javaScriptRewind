725bd676c80879470cce3568efe2fbc37d255ca7
artreysan <70738181+beartree-ars@users.noreply.github.com>
2022-08-19 22:59:09 -0500

    Emacscript modules 2

--- add.js
+++ add.js
@@ -2,3 +2,11 @@ export function add(x,y) {
     return x+y;
 }
 
+//Export for parts
+export function multiply(x,y){
+    return x*y;
+}
+export const title = 'js for react'
+export const active = true
+export const points = [12,32,13,41]
+
--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 // Ecmascript
-// Create file add.js
-// Import file in this file
-import {add} from './add.js'
+import {add,multiply,title,active,points} from './add.js'
 
-// Modify index.html from this:
-// <script src="./index.js"></script>
-
-// to this: 
-// <script type = "module" src="./index.js"></script>
-
-//Used the funtion exported
 console.log(add(10,20))
+console.log(multiply(10,20))
+console.log(active,title,points)
