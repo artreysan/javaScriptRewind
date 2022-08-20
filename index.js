813f06adf45208629c2ecf79a371e30f1bd87051
artreysan <70738181+beartree-ars@users.noreply.github.com>
2022-08-19 22:47:24 -0500

    Spread Operator

--- index.js
+++ index.js
@@ -1,47 +1,32 @@
-//Arrays methods 
-
+//Spread Operator 
 const names = ['ryan','joe','maria']
+const newNames = ['marco', 'mario', 'john']
 
-//Method 1 
-for (let i = 0; i < names.length; i++){
-    const element = names[i];
-    console.log(element);
+//Method with CONCAT 
+console.log('============================================')
+console.log(names.concat(newNames))
+
+//Spread Operation
+console.log('============================================')
+console.log([...names,...newNames])
+
+
+//Aplication
+const user = {
+    name:"ryan",
+    lastname:"ray"
+}
+ const address = {
+    street : 'main street 123',
+    city: 'mexico'
+ }
+
+
+const userInfo={
+    ...user,
+    ...address
 }
 
-//Method 1 with  forEach()
-console.log('============================================')
-names.forEach(function(name){
-    console.log(name)
-})
-
-//Method 2 with MAP
-console.log('============================================')
-const newNames = names.map(function(name){
-    return `Hola ${name}`
-})
-console.log(newNames)
-
-//Method 3 with FIND
-console.log('============================================')
-const nameFound =  names.find(function (name){
-    if(name === 'joe'){
-        return name
-    }
-})
-console.log(nameFound)
-
-//Method 4 with FILTER
-console.log('============================================')
-const nameFilter =  names.filter(function (name){
-    if(name !== 'joe'){
-        return name
-    }
-})
-console.log(nameFilter)
-
-//Method 5 with CONCAT 
-console.log('============================================')
-const newNamess = ['marco', 'mario', 'john']
-console.log(names.concat(newNamess))
-
-// More information: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array 
+console.log(user)
+console.log(address)
+console.log(userInfo)
